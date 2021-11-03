8fdba33a81c4291de4a3236c36c5cc165e8e961f
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-11-03 11:44:50 +0200

    last edit

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ v:1.0.0
 
 how tp make madlib game for the user input some word and give back pergraph about user answer 
     start - end : 3:00pm - 7:00pm
+
+running server url : null
+running client url : null
+url PR : [PR](https://github.com/yaseen1998/madlib-cli/pull/1)
--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,24 +1,33 @@
 
 import re
 def read_template(path):
+    """
+    in this function I open the txt file and return what it conten
+    """
     print("""
     ***************************************************************************************************
     *** this is game for fun will ask you to enter some words and will give back to you a paragraph ***
     ***************************************************************************************************
     """)
-    file = open(path)
-    content = "Error : Sorry file not found"
-    content = file.read() 
-    file.close()
+    file = open(path) # open txt file
+    content = file.read() # store the txt in conten
+    file.close() # close the txt filr
     return content
 
 
 
 
 def merge(path,forma):
+    """
+    format txt file to replace every {} to specific word
+    """
     return path.format(*forma)
     
 def parse_template(content):
+    """
+    in this function first I find and store key of txt file 
+    second I replace this key to {}
+    """
     # result=[]
     # path=content.split()
     # for i in path:
@@ -31,14 +40,17 @@ def parse_template(content):
 
     # return(last,tuple(arr))
 
-    data = re.findall(r"\{(.*?)\}", content)
-    text = re.sub(r"\{(.*?)\}", '{}', content)
+    data = re.findall(r"\{(.*?)\}", content) # find key of txt
+    text = re.sub(r"\{(.*?)\}", '{}', content) # replace key to {}
     return (text,tuple(data))
 
     # print(*arr)
 
 def madlib(content,replaceData):
-    replaceData=list(set(replaceData))
+    """
+    extra function just for practice another ways for format document
+    """
+    replaceData=list(set(replaceData)) # keys of txt
     content=content.split('}')
     for j in range(len(content)):
         for i in replaceData:
